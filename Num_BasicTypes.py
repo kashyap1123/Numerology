342b83937282c142614bcb2582d3639852af5e98
kashyap1123 <kashyap1123@gmail.com>
2017-12-01 09:11:08 +0530

    Add more metrics
    Add methods for Progress Number, Bad Years, Important Years.
    
    Add a placeholder for Current Lord of the Month.
    
    Add a more elegant/faster way to single. Also add a new operation: doublle.
    
    Update example to include these.

--- Num_BasicTypes.py
+++ Num_BasicTypes.py
@@ -32,12 +32,61 @@ def single(nums):
 
     return(res)
 
+# We note that singling is just the iterative mapping from {0,,10} to {0,,9}
+# as such a quick way to single is just take remainder wrt 9
+def single_quick(nums):
+    return(sum(nums)%9)
+
+
+
+def doublle(nums):
+    temp_sum    = 0
+    numsum      = sum(nums)
+    for digit in str(numsum):
+        temp_sum       += int(digit)
+    return(temp_sum)
+
+def lom(date):
+    # This needs to be implemented
+    return(0)
+
+def bad_years(dob):
+    age_latest          = 0
+    age_latest_diff     = 0
+    year_curr           = dob.year
+    imp_years           = []
+
+    while age_latest < 100:
+        age_latest_diff  = doublle([year_curr])
+        year_curr   = year_curr + age_latest_diff
+        age_latest  += age_latest_diff
+        imp_years.append(year_curr)
+
+    return(imp_years)
+
+def imp_years(dob):
+    age_latest          = 0
+    age_latest_diff     = 0
+    year_curr           = dob.year
+    imp_years           = []
+
+    while age_latest < 100:
+        age_latest_diff  = single([year_curr])
+        year_curr   = year_curr + age_latest_diff
+        age_latest  += age_latest_diff
+        imp_years.append(year_curr)
+
+    # Note that the above is essentially singling a GP and can be implemented
+    # as such. However, that lacks clarity and so we choose the above
+
+    return(imp_years)
+
 class numerologybase:
     """Class to hold all the relevant numerological metrics and details for
     an individual """
     def __init__(self, fname, lname, dob):
         self.name_first = fname
-        self.name_Last  = lname
+        self.name_last  = lname
         self.dob        = dob
         self.dob_corr   = dob.date()
         self.now        = dob.today()
@@ -50,6 +99,8 @@ class numerologybase:
         self.age            = 0
         self.birth_number   = 0
         self.birth_lom      = 0
+        self.curr_lom       = 0
+        self.progress_no    = 0
         self.bad_years      = []
         self.imp_years      = []
         self.radical_years  = []
@@ -69,6 +120,51 @@ class numerologybase:
     def pop_birthnumber(self):
         list_dob            = [self.dob_corr.day, self.dob_corr.month, (self.dob_corr.year)%100]
         self.birth_number   = single(list_dob)
+
+    # Need to complete this
+    def pop_birth_lom(self):
+        self.birth_lom      = 0
+
+    def pop_progress_number(self):
+        # Procedure laid out below is valid only for birthday (curr) till EoY
+        # For SoY till next birthday, it is (axiomatically) + 1
+        # So we compute based on preceding b'day and adjust if needed
+        adjust  = 0
+        if (self.dob_corr.month>self.today_corr.month):
+            adjust  = 1
+        elif (self.dob_corr.month==self.today_corr.month):
+            if (self.dob_corr.day>self.today_corr.day):
+                adjust  = 1
+
+        curr_year_adjust    = self.today_corr.year - adjust
+
+        # Step 1. Repeat Birth Number proc. with current year
+        a1  = single([self.dob_corr.day, self.dob_corr.month, (curr_year_adjust)%100])
+        # Step 2. Subtract YoB from current years. Note this is not always=age
+        a2  = curr_year_adjust - self.dob_corr.year
+        # Step 3. Double this and single digits
+        a3  = single([a2 * 2])
+        # Step 4. Add this to YoB
+        a4  = self.dob_corr.year + a3
+        # Step 5. Single the digits of the year
+        a5  = single([a4])
+        # Step 6. Add a1 and a5
+        a6  = a1 + a5
+
+        # Now adjust if needed
+        self.progress_no    = single([a6 + adjust])
+        # Can write a test to check that progress numbers increase by 3 every yr
+
+    def pop_curr_lom(self):
+        # First make sure progress_no is computed, we need it
+        self.pop_progress_number()
+        self.curr_lom   = single([self.progress_no, lom(self.today_corr)])
+
+    def pop_bad_years(self):
+        self.bad_years  = bad_years(self.dob_corr)
+
+    def pop_imp_years(self):
+        self.imp_years  = imp_years(self.dob_corr)
 
 # for debug, remove once done
 print("done")
--- Num_BasicUse.py
+++ Num_BasicUse.py
@@ -8,13 +8,23 @@ An example script to show basic use case for the numerology class
 from Num_BasicTypes import numerologybase
 from datetime import datetime
 
-fname   = "Manjunath Kashyap"
+# fname   = "Manjunath Kashyap"
+# lname   = "Jataprolu"
+# dob     = datetime(1988, 10, 03, 15, 25)
+fname   = "Vaishnavi Kasyap"
 lname   = "Jataprolu"
-dob     = datetime(1988, 10, 03, 15, 25)
+dob     = datetime(1991, 11, 18, 22, 30)
 
 num_object  = numerologybase(fname, lname, dob)
+print(num_object.name_first, num_object.name_last)
 num_object.pop_dob_today_corr()
 num_object.pop_age()
 print('Age is ', num_object.age)
 num_object.pop_birthnumber()
 print('Birth Number is ', num_object.birth_number)
+num_object.pop_progress_number()
+print('Progress Number is ', num_object.progress_no)
+num_object.pop_bad_years()
+print('Bad years are ', num_object.bad_years)
+num_object.pop_imp_years()
+print('Imp years are ', num_object.imp_years)
